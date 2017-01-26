9d151d83c82985cbfecd132594d3b96f16f8f69a
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2017-01-26 08:31:59 +0100

    Squelette TP1 et TP2

---
+++ tp1/listes.pl
@@ -0,0 +1,51 @@
+/*
+  Prénom1 Nom1
+  Prénom2 Nom2
+*/
+
+/* Question 1 */
+
+/* Question 2 */
+
+/* Question 3 */
+
+/* Question 4 */
+
+/* Question 5 */
+
+/* Question 6 */
+
+/* Question 7 */
+
+/* Question 8 */
+
+/* Tris sur les listes */
+
+/* Question 9 */
+
+/* Question 10 */
+
+/* Question 11 */
+
+/* Question 12 */
+
+/* Question 13 */
+
+/* Question 14 */
+
+/* Question 15 */
+
+/* Opérations de base sur les ensembles */
+
+/* Question 16 */
+
+/* Question 17 */
+
+/* Question 18 */
+
+/* Question 19 */
+
+/* Question 20 */
+
+/* Question 21 */
+
