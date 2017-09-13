fdaf80ab3523cb6584d2cb0eded8dc4beccc6dde
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-09-13 16:56:05 +0200

    maccess

--- access/maccess.c
+++ access/maccess.c
@@ -1,9 +1,7 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <limits.h>
+#
+include < unistd.h > #include < stdio.h > #include < limits.h > #include < errno.h >
 
-int vflag, rflag, wflag, xflag;
-
+  int vflag, rflag, wflag, xflag;
 
 /* affiche la valeur des constantes NAME_MAX et PATH_MAX */
 void prlimit() {
@@ -11,48 +9,81 @@ void prlimit() {
   printf("%d\n", PATH_MAX);
 }
 
-
-void usage (char *prog) {
-   printf("usage : %s [rvwx] file", prog);
+void usage(char * prog) {
+  printf("usage : %s [rvwx] file\n", prog);
 }
 
-int maccess (int argc, char **argv) {
-   int i;
-   printf("OPTIONS verbose:%i, read:%i, write:%i, exec:%i\n",vflag,rflag,wflag,xflag);
-   printf("REMAINING %i ARGS:\n",argc);
-   for (i = 0; i < argc; i++) {
-      printf("\t%s\n",argv[i]);
-   }
-   return 1;
-}
+/* test les droits rwx sur un ou plusieurs fichiers */
+int maccess(int argc, char * * argv) {
+  int i, mode, result;
 
+  /* if the user didn't specify any permissions to test, exit */
+  if (!rflag && !wflag && !xflag)
+  {
+    usage("maccess");
+    return 1;
+  }
 
-int main (int argc, char **argv) {
-   int ch;
-   vflag = 0;
-   rflag = 0;
-   wflag = 0;
-   xflag = 0;
-   while ((ch = getopt(argc, argv, "vrwxh")) != -1) {
-      switch (ch) {
-      case 'v':
-         vflag = 1;
-         break;
-      case 'r':
-         rflag = 1;
-         break;
-      case 'w':
-         wflag = 1;
-         break;
-      case 'x':
-         xflag = 1;
-         break;
-      case 'h':
-      default:
-         usage(argv[0]);
+  printf("OPTIONS verbose:%i, read:%i, write:%i, exec:%i\n", vflag, rflag, wflag, xflag);
+  printf("REMAINING %i ARGS:\n", argc);
+  for (i = 0; i < argc; i++)
+  {
+    printf("\t%s\n", argv[i]);
+
+    mode = 0;
+    if (rflag)
+    {
+      mode = mode | R_OK;
+    }
+    if (wflag)
+    {
+      mode = mode | W_OK;
+    }
+    if (xflag)
+    {
+      mode = mode | X_OK;
+    }
+
+    result = access(argv[i], mode);
+    if (result)
+    {
+      if (vflag) {
+        perror("erreur");
       }
-   }
-   argc -= optind;
-   argv += optind;
-   return maccess(argc,argv);
+      return 1;
+    } else {
+      printf("%s\n", "acces confirmé");
+    }
+  }
+  return result; /* last result is the global result for all the files */
+}
+
+int main(int argc, char * * argv) {
+  int ch;
+  vflag = 0;
+  rflag = 0;
+  wflag = 0;
+  xflag = 0;
+  while ((ch = getopt(argc, argv, "vrwxh")) != -1) {
+    switch (ch) {
+    case 'v':
+      vflag = 1;
+      break;
+    case 'r':
+      rflag = 1;
+      break;
+    case 'w':
+      wflag = 1;
+      break;
+    case 'x':
+      xflag = 1;
+      break;
+    case 'h':
+    default:
+      usage(argv[0]);
+    }
+  }
+  argc -= optind;
+  argv += optind;
+  return maccess(argc, argv);
 }
