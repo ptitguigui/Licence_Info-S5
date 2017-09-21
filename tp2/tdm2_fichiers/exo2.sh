3c8c78ec3e0e7eeb72722d7c83cbb24c97a47216
Guillaume <guillaume@debian.home>
2017-09-21 17:39:22 +0200

    tp finis

--- tp2/tdm2_fichiers/exo2.sh
+++ tp2/tdm2_fichiers/exo2.sh
@@ -4,20 +4,18 @@ refEntite="&\w*;"
 baliseOuvrante="<$nomXML[[:space:]](*$nomXML\=$valeurAttribut[[:space:]]?)*>"
 
 
-numero33='+33[[:space:]](0)[[:space:]][0-9].([0-9]{2}.[0-9]{2}){2}'
+numero33="(+33[[:space:]]\(0\)[[:space:]][1-9].[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2})"
+numeroNormal="(0[0-9].[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2})"
+numeros="$numero33|$numeroNormal"
 
-echo -e "\n** Q1 :\n"
+echo -e "\n** Q1 (avec un compte 10):\n"
 
 egrep -m 10 $valeurAttribut html/* --color=auto
 
-echo -e "\n** Q2 :\n"
+echo -e "\n** Q2 (avec un compte 10):\n"
 
 egrep -m 10 $baliseOuvrante html/* --color=auto
 
 echo -e "\n** Q3 :\n"
 
-egrep  $numero33 html/* --color=auto
-
-echo -e "\n** Q4 :\n"
-
-#egrep  ' ' html/* --color=auto
+egrep -o $numeros html/* --color=auto
