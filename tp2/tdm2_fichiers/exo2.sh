fe1259e89571e3a9bf1b48f9e00e2405890637f4
Lepretre Guillaume <lepretreg@a12p15.fil.univ-lille1.fr>
2017-09-21 11:51:43 +0200

    lol

--- tp2/tdm2_fichiers/exo2.sh
+++ tp2/tdm2_fichiers/exo2.sh
@@ -3,6 +3,9 @@ nomXML="([[:alpha:]]|:|_)([[:alnum:]]|:|_|:|-|\.)*"
 refEntite="&\w*;"
 baliseOuvrante="<$nomXML[[:space:]](*$nomXML\=$valeurAttribut[[:space:]]?)*>"
 
+
+numero33='+33[[:space:]](0)[[:space:]][0-9].([0-9]{2}.[0-9]{2}){2}'
+
 echo -e "\n** Q1 :\n"
 
 egrep -m 10 $valeurAttribut html/* --color=auto
@@ -13,7 +16,7 @@ egrep -m 10 $baliseOuvrante html/* --color=auto
 
 echo -e "\n** Q3 :\n"
 
-#egrep  ' ' html/* --color=auto
+egrep  $numero33 html/* --color=auto
 
 echo -e "\n** Q4 :\n"
 
