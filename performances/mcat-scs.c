9eb0324963cfef3d1e477d1943c7f1eee2ea9203
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-09-27 16:53:30 +0200

    cat statique

---
+++ performances/mcat-scs.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void mcat_scd(char *pathname)
+{
+  int fdRead;
+  int nbLu;
+  int nbOctets;
+  char *contenu;
+
+  nbOctets = 4096;
+  contenu = (char*) malloc(nbOctets * sizeof(char));
+
+  fdRead = open(pathname, O_RDONLY);
+  assert(fdRead != -1);
+
+  while ( (nbLu = read(fdRead, contenu, nbOctets)) > 0 )
+  {
+    write(STDOUT_FILENO, contenu, nbLu);
+  }
+  close(fdRead);
+  free(contenu);
+}
+
+
+int main(int argc, char **argv)
+{
+  assert(argc == 2);
+  mcat_scd(argv[1]);
+  return 0;
+}
