50cf4290c8d6a6f2be1a650d8be2f019efdaed3e
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-09-27 16:28:42 +0200

    test script uses command

--- performances/mcat.sh
+++ performances/mcat.sh
@@ -18,16 +18,9 @@ TIME_OPT="-f %e %U %S"
 # initialisation du fichier de résultats
 rm -f $TIME_FILE && echo "# real user sys" > $TIME_FILE
 
-# un exemple de boucle
-for str in  foo bar gee ; do
-    echo $str
-done
 
-# un autre exemple de boucle
 for size in `awk 'BEGIN { for( i=1; i<=8388608; i*=2 ) print i }'`; do
     export MCAT_BUFSIZ=$size
-    echo $MCAT_BUFSIZ
+    $TIME_CMD "$TIME_OPT" $MCAT $MCAT_INPUT > /dev/null 2>> $TIME_FILE
 done
 
-# un exemple de redirection des sorties standard et d'erreur
-$TIME_CMD "$TIME_OPT" ls > /dev/null 2>> $TIME_FILE
