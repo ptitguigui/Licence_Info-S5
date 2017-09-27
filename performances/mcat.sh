e98290d439352547cb24eb696fa9b54775ee21f2
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-09-27 16:47:02 +0200

    script adapté pour gnuplot

--- performances/mcat.sh
+++ performances/mcat.sh
@@ -13,14 +13,15 @@ TIME_FILE=mcat-tm.dat
 # la commande gnu time
 TIME_CMD="/usr/bin/time"
 # les options de cette commande
-TIME_OPT="-f %e %U %S"
+TIME_OPT="-f %e\t%U\t%S"
 
 # initialisation du fichier de résultats
-rm -f $TIME_FILE && echo "# real user sys" > $TIME_FILE
+rm -f $TIME_FILE && echo "# buffsize\treal\tuser\tsys" > $TIME_FILE
 
 
 for size in `awk 'BEGIN { for( i=1; i<=8388608; i*=2 ) print i }'`; do
     export MCAT_BUFSIZ=$size
+    echo -n "\t$size\t" >> $TIME_FILE
     $TIME_CMD "$TIME_OPT" $MCAT $MCAT_INPUT > /dev/null 2>> $TIME_FILE
 done
 
