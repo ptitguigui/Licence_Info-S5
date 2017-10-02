4fb08d846ff22950eb3e646e770d82e5c0a3958e
Lepretre Guillaume <lepretreg@a14p15.fil.univ-lille1.fr>
2017-10-02 16:20:44 +0200

    impaire jusque 11

---
+++ TP3/impair.sql
@@ -0,0 +1,36 @@
+--Q1
+ \echo 'Question 1'
+select Max(prix)
+from catalogue;
+--Q3
+ \echo 'Question 3'
+select fnom
+from catalogue natural join fournisseurs
+where prix >=(select max(prix) from catalogue);
+--Q5
+ \echo 'Question 5'
+select  anom, count(f.fid) as nb_fournisseur, min(prix), max(prix)
+from articles as a, fournisseurs as f, catalogue as c
+where a.aid = c.aid and f.fid = c.fid
+group by anom
+having count(f.fid) > 1;
+--Q7
+ \echo 'Question 7'
+select acoul as COULEUR, avg(prix) as PRIX_MOYEN
+from articles natural join catalogue
+group by acoul
+order by PRIX_MOYEN asc;
+--Q9
+\echo 'Question 9'
+select  anom, count(f.fid) as nb_fournisseur
+from articles as a, fournisseurs as f, catalogue as c
+where a.aid = c.aid and f.fid = c.fid
+group by anom
+--Q11
+\echo 'Question 11'/*
+select fnom, anom
+from articles as a, fournisseurs as f, catalogue as c
+where a.aid = c.aid and f.fid = c.fid
+group by fnom;*/
+--Q13
+\echo 'Question 11'
