aa02f77672dbd8845c48e6dfb8bff999455349bc
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-10-03 11:15:30 +0200

    fix ResourcePool constructor

--- src/main/java/fil/coo/ResourcePool.java
+++ src/main/java/fil/coo/ResourcePool.java
@@ -1,11 +1,28 @@
 package fil.coo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 public abstract class ResourcePool<T extends Resource> {
 
-    List<T> resourceList;
+    protected int nbResources;
+
+    protected List<T> resourceList;
+
+    public ResourcePool(int nbResources) {
+        this.nbResources = nbResources;
+        initResources();
+    }
+
+    private void initResources() {
+        resourceList = new ArrayList<T>();
+        for (int i=0;i<nbResources;i++) {
+            T resource = createOneResource();
+        }
+    }
+
+    protected abstract T createOneResource();
 
     abstract T provideResource() throws NoSuchElementException;
 
