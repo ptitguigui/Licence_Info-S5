32f064e9c17145a162d96e1cbeeaa9c9c7d30869
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2017-10-07 18:04:27 +0200

    new tail_simpliste

--- tail/mtail.c
+++ tail/mtail.c
@@ -1,213 +1,190 @@
+
 #include <assert.h>
+#include <getopt.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <sys/fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
 
 /**
  *
- * @param fd the file descriptor of the file to print
- * @param ligne_debut the number of the line to print from
- * @param read_offset the offset from which we will start reading to dertermine where ligne_debut is
+ * @param fd the file descriptor for which we want to count the lines for
+ * @return -1 if the file is empty, or the number of lines
  */
-void print_file(int fd, int ligne_debut, int read_offset) {
-    char *readBuffer;
-    char *printBuffer;
+int count_total_lines(int fd)
+{
+    int nb_lines = -1;
+    char *buffer;
+    int bufferSize;
     int status;
-    int nb_lignes_lus;
     int i;
-    int print_offset;
 
     assert(fd != -1);
-    assert(ligne_debut >= 0);
-    i = 0;
-    status = 0;
-    nb_lignes_lus = 0;
-    print_offset = 0;
 
-    readBuffer = malloc(4096 * sizeof(char));
-    printBuffer = malloc(4096 * sizeof(char));
+    if (!lseek(fd, 0, SEEK_END))
+    {
+        return -1;
+    }
 
-    lseek(fd, read_offset, SEEK_SET);
+    lseek(fd, 0, SEEK_SET);
 
+    nb_lines = 0;
+    bufferSize = 4096;
+    buffer = (char *) malloc(bufferSize * sizeof(char));
 
-    /*
-     * Set a variable "print index", the index of the byte from which we will print
-     * "newline print number" => the number of newline from which we will print
-     * For the whole file, read in chunks of 4096 bytes.
-     * For each chunk, if the number of newlines found is less than the number of the newline print number:
-     *      scan each char =>
-     *           If we encounter a newline, increment the number of newlines found
-     *           If the number of newlines found equals the number of the newline from which we need to print
-     *                  add the byte position of the (newline+1) to the print index.
-     * If we don't encounter a newline in the whole chunk
-     *      add the number of bytes in the chunk to the print index.
-     */
-    while ((status = read(fd, readBuffer, 4096)) && nb_lignes_lus < ligne_debut) {
-        while (i < status && nb_lignes_lus < ligne_debut) {
-            if (readBuffer[i] == '\n') {
-                nb_lignes_lus++;
-                if (nb_lignes_lus == ligne_debut) {
-                    print_offset += i + 1;
-                }
+    while ((status = read(fd, buffer, bufferSize)))
+    {
+        /*
+        printf("did read %d bytes: \"%.*s\"", status, status, buffer);
+         */
+        i = 0;
+        while (i < status)
+        {
+            if (buffer[i] == '\n')
+            {
+                nb_lines++;
             }
             i++;
         }
-        if (nb_lignes_lus < ligne_debut) {
-            print_offset += status;
-        }
     }
-
-    lseek(fd, print_offset + read_offset, SEEK_SET);
-    while ((status = read(fd, printBuffer, 4096))) {
-        printf("%s", printBuffer);
-    }
-
-    close(fd);
-    free(readBuffer);
-    free(printBuffer);
+    return nb_lines;
 }
-
-/*
- * Counts the number of lines, then calls print_file() with the index of where to start printing
- */
-int mtail(const char *path, int nb_lignes_voulu) {
-    char contenu[4096];
-    int status;
-    int nb_lignes_total;
-    int fd;
-    int i;
-
-    int ligne_debut;
-
-    nb_lignes_total = 0;
-
-    fd = open(path, O_RDONLY);
-    assert(fd != -1);
-
-    if (!nb_lignes_voulu) {
-        print_file(fd, 0, 0);
-        return 0;
-    }
-
-    while ((status = read(fd, contenu, 4096))) {
-        for (i = 0; i < status; i++) {
-            if (contenu[i] == '\n') {
-                nb_lignes_total++;
-            }
-        }
-    }
-
-    /*
-     * nb_lignes_total already contains (separators+1) because of how files are read
-     */
-
-    if (nb_lignes_total < nb_lignes_voulu) {
-        ligne_debut = 0;
-    } else {
-        ligne_debut = nb_lignes_total - nb_lignes_voulu;
-    }
-
-    print_file(fd, ligne_debut, 0);
-    return 0;
-}
-
 
 /**
  *
- * @param fd the file descriptor to tail
- * @param size_buffer  the size of the buffer
- * @param ntail the number of last lines
- * @return 0 if success
+ * Resets the read offset of the file and reads the file until the last newline, where the next character will belong to wanted_line_index.
+ *
+ * @param fd the file descriptor for which we want to count the lines for
+ * @param wanted_line_index the index of the line that we want
+ * @return the offset for the wanted line or -1 if failed to find
  */
-int tail_last_lines(int fd, int *size_buffer, int ntail) {
+int seek_until_line(int fd, int wanted_line_index)
+{
+    /**
+     * index of the current line, starts at 0. Incremented immediately once a '\n' is found
+     */
+    int current_line_index;
+    char currentChar;
     int status;
-    int total_newline;
     int i;
-    int total_size_file;
+    int offset;
+
+    /*
+     * If the wanted line is the first line (index 0), the offset value for this line is 0;
+     */
+    if (!wanted_line_index)
+    {
+        return 0;
+    }
+
+    assert(fd != -1);
+    current_line_index = 0;
+    offset = 0;
+
+    lseek(fd, 0, SEEK_SET);
+
+    while (current_line_index < wanted_line_index && (status = read(fd, &currentChar, 1)))
+    {
+        i = 0;
+        while (i < status)
+        {
+            if (currentChar == '\n')
+            {
+                current_line_index++;
+            }
+            i++;
+        }
+        offset++;
+    }
+    if (current_line_index == wanted_line_index)
+    {
+        /*
+        printf("current line index: %d\n", current_line_index);
+        printf("offset for wanted line : %d\n", offset);
+         */
+        return offset;
+    }
+    return -1;
+}
+
+/**
+ *
+ * @param fd the file descriptor for which we want to count the lines for
+ * @param start_line_index the index of the line that we will start printing at
+ */
+void print_file_from_line(int fd, int start_line_index)
+{
     char *buffer;
+    int status;
+    int bufferSize;
 
-    assert(fd != -1);
-    assert(*size_buffer > 0);
 
-    status = 0;
-    total_newline = 0;
-    i = 0;
-    total_size_file = 0;
+    seek_until_line(fd, start_line_index);
 
-    total_size_file = lseek(fd, 0, SEEK_END);
+    bufferSize = 4096;
+    buffer = (char *) malloc(bufferSize * sizeof(char));
 
     /*
-    if the buffer is bigger than the file's size, print everything
-    */
-    if (*size_buffer > total_size_file) {
-        /*
-        printf("buffer bigger than filesize, will print everything\n");
-         */
-        print_file(fd, 0, 0);
-        return 0;
-    }
-
-    buffer = malloc(*size_buffer * sizeof(char));
-
-
-    lseek(fd, -*size_buffer, SEEK_END);
-    while ((status = read(fd, buffer, *size_buffer))) {
-        for (i = 0; i < status; i++) {
-            if (buffer[i] == '\n') {
-                total_newline++;
-            }
-        }
-        /*
-        printf("with buffer %d, read: \"%s\"\n", *size_buffer, buffer);
-         */
-    }
-
-    /*
-    printf("found %d newlines\n", total_newline);
+    printf("start actual printing\n");
      */
 
-    if (total_newline < ntail) {
-        *size_buffer *= 2;
-        return tail_last_lines(fd, size_buffer, ntail);
-    } else {
-        print_file(fd, total_newline - ntail, total_size_file - *size_buffer);
+    while ((status = read(fd, buffer, bufferSize)))
+    {
+        printf("%.*s", status, buffer);
     }
 
-    close(fd);
-    free(buffer);
-    return 0;
 }
 
 /**
  *
- * @param path  the path of the file to tail
- * @param ntail the number of last lines
- * @return 0 if success
+ * Counts the number of total lines to start printing from (total-ntail)
+ *
+ * @param path the path to the file we want to read
+ * @param ntail the number of last lines we want to print
+ * @return 0 if no error
  */
-int tail_efficace(const char *path, int ntail) {
+int tail_simpliste(char *path, int ntail)
+{
     int fd;
-    int size_buffer;
-
-    size_buffer = 4;
+    int total_lines;
+    /**
+     * the index of the line that we will start printing with
+     */
+    int start_print_line_index;
 
     fd = open(path, O_RDONLY);
     assert(fd != -1);
-    return tail_last_lines(fd, &size_buffer, ntail);
+
+    total_lines = count_total_lines(fd);
+    /*
+    printf("found %d lines\n", total_lines);
+    empty file if (lines == -1)
+    */
+
+    if (total_lines)
+    {
+        start_print_line_index = total_lines - ntail;
+        print_file_from_line(fd, start_print_line_index);
+    }
+    return 0;
 }
 
 /*
  * Accepts ./mtail -n N file
  */
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     char ch;
     int nb_lignes;
 
     assert(argc == 2 || argc == 4);
     nb_lignes = 0;
 
-    while ((ch = getopt(argc, argv, "n:")) != -1) {
-        switch (ch) {
+    while ((ch = getopt(argc, argv, "n:")) != -1)
+    {
+        switch (ch)
+        {
             case 'n':
                 nb_lignes = atoi(optarg);
                 break;
@@ -216,5 +193,5 @@ int main(int argc, char **argv) {
         }
     }
 
-    return tail_efficace(argv[3], nb_lignes);
+    return tail_simpliste(argv[3], nb_lignes);
 }
