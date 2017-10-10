38296cef29a30a5acb2d723026569258fbd77e71
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-10-10 10:47:57 +0200

    Swimmer skeleton for take/free

--- src/main/java/fil/coo/client/Swimmer.java
+++ src/main/java/fil/coo/client/Swimmer.java
@@ -1,14 +1,31 @@
 package fil.coo.client;
 
+import fil.coo.actions.FreeResourceAction;
+import fil.coo.actions.TakeResourceAction;
 import fil.coo.actions.scheduler.SequentialScheduler;
 import fil.coo.client.interfaces.ResourceUser;
 import fil.coo.resource.Basket;
 import fil.coo.resource.Cubicle;
+import fil.coo.resource.pools.ResourcePool;
 
 public class Swimmer extends SequentialScheduler {
 
     private ResourceUser<Basket> basketResourceUser;
     private ResourceUser<Cubicle> cubicleResourceUser;
+
+    /*
+    private ResourcePool<Basket> basketPool;
+    private ResourcePool<Cubicle> cubiclePool;
+
+    private void initResourceUsers() {
+        TakeResourceAction<Basket> basketTakeResourceAction = new TakeResourceAction<>(basketResourceUser, basketPool);
+        TakeResourceAction<Cubicle> cubicleTakeResourceAction= new TakeResourceAction<>(cubicleResourceUser,
+                cubiclePool);
+
+        FreeResourceAction<Basket> basketFreeResourceAction = new FreeResourceAction<>(basketResourceUser, basketPool);
+        FreeResourceAction<Cubicle> cubicleFreeResourceAction= new FreeResourceAction<>(cubicleResourceUser,
+                cubiclePool);
+    */
 
 
 }
