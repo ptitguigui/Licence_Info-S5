3111c63808bae993a07f63f73acb9173df43994e
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-10-18 17:25:02 +0200

    -k

--- do/do.c
+++ do/do.c
@@ -6,9 +6,19 @@
 #include <sys/types.h>
 #include <wait.h>
 
-static int conjunction;
-static int disjunction;
-static int short_circuit;
+static int conjunction_opt;
+static int disjunction_opt;
+static int short_circuit_opt;
+static int kill_children_opt;
+
+void kill_children(int nb_pids, int pids[])
+{
+  int i;
+  for (i=0;i<nb_pids;i++)
+  {
+  kill(pids[i], SIGKILL);
+  }
+}
 
 /*
 returns:
@@ -22,10 +32,13 @@ int mdo(int argc, char **argv)
   int i;
   int return_value;
   char *single_arg[2];
+  int *children_pids;
 
   status = (int*) malloc(argc * sizeof(int));
+  children_pids = (int*) malloc(argc * sizeof(int));
+
   single_arg[1] = NULL;
-  return_value = conjunction ? 1 : 0;
+  return_value = conjunction_opt ? 1 : 0;
 
   for (i=0;i<argc;i++)
   {
@@ -35,31 +48,46 @@ int mdo(int argc, char **argv)
         case -1:
           exit(EXIT_FAILURE);
         case 0:
+          /*
           printf("will exc %s\n", argv[i]);
+          */
+
           single_arg[0] = argv[i];
           execvp(argv[i], single_arg);
           exit(EXIT_FAILURE);
         default:
-          waitpid(-1, &status[i], 0);
-
-          if (i == 0)
-          {
-            return_value = WEXITSTATUS(status[i]);
-          } else if (disjunction)
-          {
-            return_value &= WEXITSTATUS(status[i]);
-          } else if (!disjunction){
-            return_value |= WEXITSTATUS(status[i]);
-          }
-
-          if (short_circuit && disjunction && !return_value)
-          {
-            exit(EXIT_SUCCESS);
-          } else if (short_circuit && conjunction && return_value) {
-            exit(EXIT_FAILURE);
-          }
+          children_pids[i] = pid;
+          break;
       }
   }
+
+  for (i=0;i<argc;i++)
+  {
+    waitpid(-1, &status[i], 0);
+    /*
+    printf("waited for pid %d, index %d with status %d\n", wait_pid_result, i, WEXITSTATUS(status[i]));
+    */
+
+    if (i == 0 && !short_circuit_opt)
+    {
+      return_value = WEXITSTATUS(status[i]);
+    } else if (!short_circuit_opt && disjunction_opt)
+    {
+      return_value &= WEXITSTATUS(status[i]);
+    } else if (!short_circuit_opt && !disjunction_opt){
+      return_value |= WEXITSTATUS(status[i]);
+    }
+
+    if (kill_children_opt && disjunction_opt && !return_value)
+    {
+      kill_children(argc, children_pids);
+      exit(EXIT_SUCCESS);
+    } else if (short_circuit_opt && conjunction_opt && return_value) {
+      kill_children(argc, children_pids);
+      exit(EXIT_FAILURE);
+    }
+  }
+
   return return_value;
 }
 
@@ -68,33 +96,37 @@ int main(int argc, char **argv)
   char ch;
   int nb_opts_found;
   /* conjonction par défaut */
-  conjunction = 0;
-  short_circuit = 0;
+  conjunction_opt = 0;
+  short_circuit_opt = 0;
   nb_opts_found = 0;
 
-  while ((ch = getopt(argc, argv, "aoc")) != -1)
+  while ((ch = getopt(argc, argv, "aock")) != -1)
   {
     switch (ch) {
     case 'a':
-      conjunction = 1;
+      conjunction_opt = 1;
       nb_opts_found++;
       break;
     case 'o':
-      disjunction = 1;
+      disjunction_opt = 1;
       nb_opts_found++;
       break;
     case 'c':
-      short_circuit = 1;
+      short_circuit_opt = 1;
+      nb_opts_found++;
+      break;
+    case 'k':
+      kill_children_opt = 1;
       nb_opts_found++;
       break;
     }
   }
 
-  if (!conjunction && !disjunction)
-    conjunction = 1;
+  if (!conjunction_opt && !disjunction_opt)
+    conjunction_opt = 1;
 
   /* on ne peut pas avoir les deux en même temps */
-  assert( !(conjunction && disjunction));
+  assert( !(conjunction_opt && disjunction_opt));
 
   return mdo(argc-nb_opts_found-1, argv+nb_opts_found+1);
 }
