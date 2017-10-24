776133ec5828fe265ccd9ac6266e3460f5779c0f
Lepretre Guillaume <lepretreg@a15p15.fil.univ-lille1.fr>
2017-10-24 11:19:51 +0200

    yes no class

--- src/main/java/fil/coo/answers/YesNoAnswer.java
+++ src/main/java/fil/coo/answers/YesNoAnswer.java
@@ -2,13 +2,15 @@ package fil.coo.answers;
 
 public class YesNoAnswer extends SingleAnswer {
 	
+	public YesNoAnswer(String answer) {
+		super(answer);
+	}
+
 	public String getPrompt() {
-		// TODO Auto-generated method stub
-		return null;
+		return "(oui/non)" + this.getAnswer() ;
 	}
 
 	public boolean isValid(String userAnswer) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return false;
+		return userAnswer.equals("oui") || userAnswer.equals("non");
 	}
 }
---
+++ src/test/java/fil/coo/answers/YesNoAnswerTest.java
@@ -0,0 +1,20 @@
+package fil.coo.answers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class YesNoAnswerTest {
+
+	
+	@Test
+	public void testGetPrompt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValid() {
+		fail("Not yet implemented");
+	}
+
+}
