7607a3071a47523620a9ac7e94033fc0c10b1d99
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-10-25 14:29:41 +0200

    fix SingleAnswerTest

--- src/test/java/fil/coo/answers/SingleAnswerTest.java
+++ src/test/java/fil/coo/answers/SingleAnswerTest.java
@@ -3,7 +3,7 @@ package fil.coo.answers;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public abstract class SingleAnswerTest extends AnswerTest {
 
@@ -14,23 +14,29 @@ public abstract class SingleAnswerTest extends AnswerTest {
         singleAnswer = getSpecificSingleAnswer(getDefaultAnswer());
     }
 
+    /**
+     * @return a default answer for the implementing Answer class
+     */
     protected abstract String getDefaultAnswer();
 
+    /**
+     * @param answer the correct answer
+     * @return an instance of the implementing Answer to test
+     */
     protected abstract SingleAnswer getSpecificSingleAnswer(String answer);
 
     @Override
     public Answer getSpecificAnswer() {
-        return getSpecificSingleAnswer("answer");
+        return getSpecificSingleAnswer(getDefaultAnswer());
     }
 
-    @Test(expected=NullPointerException.class)
-    public void testWhenAnwserIsNull() {
+    @Test(expected = NullPointerException.class)
+    public void testNullConstructionThrows() {
         SingleAnswer s = getSpecificSingleAnswer(null);
     }
 
     @Test
     public void testGetSetWhenOK() {
-        assertNotNull(singleAnswer.getAnswer());
         String dummyAnswer = "dummy_answer";
         SingleAnswer oneAnswer = getSpecificSingleAnswer(dummyAnswer);
         assertEquals(dummyAnswer, oneAnswer.getAnswer());
