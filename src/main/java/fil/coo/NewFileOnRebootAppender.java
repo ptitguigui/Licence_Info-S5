afd85406d792338bf83d652bda848634a8152107
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-11-07 11:23:58 +0100

    new logger

---
+++ src/main/java/fil/coo/NewFileOnRebootAppender.java
@@ -0,0 +1,20 @@
+package fil.coo;
+
+import org.apache.log4j.FileAppender;
+
+import java.time.LocalDateTime;
+
+public class NewFileOnRebootAppender extends FileAppender {
+
+    public NewFileOnRebootAppender() {
+    }
+
+    @Override
+    public void setFile(String file) {
+        super.setFile(prependDate(file));
+    }
+
+    private static String prependDate(String filename) {
+        return filename + "_" + LocalDateTime.now().toString();
+    }
+}
