4902a22c029d05da7017fca7692855b00454de12
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-11-08 17:29:30 +0100

    second attempt

--- pipe.c
+++ pipe.c
@@ -14,16 +14,19 @@ void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
   pid_t ch;
   int j;
 
+  fd = calloc(2, (nbcmd-1) * sizeof(int));
+  assert(fd);
 
   for (i=0;i<nbcmd;i++) {
     while (cmds[i][j] != NULL)
     {
-      printf("cmd[%d][%d] :%s\n",i , j, cmds[i][j]);
+      printf("cmd[%d][%d] :",i , j);
+      printf("%s\n",cmds[i][j]);
+      fflush(stdout);
       j++;
     }
+    j=0;
   }
-
-  fd = malloc(2 * (nbcmd-1) * sizeof(int));
 
   pipe(fd[0]);
   assert(status == 0);
@@ -32,7 +35,7 @@ void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
   if (ch == 0)
   {
     close(fd[0][0]);
-    dup2(fd[0][1], STDOUT_FILENO);
+    assert(dup2(fd[0][1], STDOUT_FILENO) != -1);
     close(fd[0][1]);
 
     execvp(cmds[0][0], cmds[0]);
@@ -43,7 +46,7 @@ void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
   if (ch == 0)
   {
     close(fd[0][1]);
-    dup2(fd[0][0], STDIN_FILENO);
+    assert(dup2(fd[0][0], STDIN_FILENO) != -1);
     close(fd[0][0]);
 
     execvp(cmds[1][0], cmds[1]);
