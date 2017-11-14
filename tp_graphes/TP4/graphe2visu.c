dbba51149e717d472da389f925f2a19d6b460e29
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-11-14 15:34:38 +0100

    parcours profondeur

--- tp_graphes/TP4/graphe2visu.c
+++ tp_graphes/TP4/graphe2visu.c
@@ -4,9 +4,83 @@
 
 #include "graphe.h"
 
+/* Couleurs */
+typedef enum {ROUGE=0, BLEU=1, VERT=2} tCouleur;
+typedef tCouleur tTabCouleurs[MAX_SOMMETS];
 
-void plusCourteChaine(tGraphe graphe, char *nom_depart)
+/*
+graphe: le graphe a parcourir
+tabCouleurs: le tableau ou stocker les Couleurs
+couleur: la couleur à stocker
+num_exclu: le numero du sommet a exclure
+*/
+void colorieSauf(tGraphe graphe, tTabCouleurs tabCouleurs, tCouleur couleur, int nom_exclu)
 {
+  int i;
+  int nbSommets;
+
+  nbSommets = grapheNbSommets(graphe);
+  for (i=0;i<nbSommets;i++)
+  {
+    if (i != nom_exclu)
+    {
+      tabCouleurs[i] = couleur;
+    }
+  }
+}
+
+
+void parcours_profondeur(tGraphe graphe, char *nom_depart)
+{
+  tNumeroSommet s;
+  tPileSommets pile;
+  /*
+  tabCouleurs[i] correspond à la couleur du sommet i
+  */
+  tTabCouleurs tabCouleurs;
+
+  tNumeroSommet x;
+  int i;
+  int nbVoisins;
+  tNumeroSommet y;
+  int possedeVoisinBleu;
+
+  s = grapheChercheSommetParNom(graphe, nom_depart);
+  pile = pileSommetsAlloue();
+
+  colorieSauf(graphe, tabCouleurs, BLEU, s);
+
+  tabCouleurs[s] = VERT;
+  pileSommetsEmpile(pile, s);
+
+  while (!pileSommetsEstVide(pile))
+  {
+    x = pileSommetsTete(pile);
+    possedeVoisinBleu = 0;
+
+    nbVoisins = grapheNbVoisinsSommet(graphe, x);
+    for (i=0;i<nbVoisins;i++)
+    {
+      y = grapheVoisinSommetNumero(graphe, x, i);
+      if (tabCouleurs[y] == BLEU)
+      {
+        possedeVoisinBleu = 1;
+        tabCouleurs[y] = VERT;
+        pileSommetsEmpile(pile, y);
+      }
+    }
+
+    if (possedeVoisinBleu == 0)
+    {
+      tabCouleurs[x] = ROUGE;
+      /*
+      c'est forcément x qui est dépilé puisqu'on n'a pas touché à la pile quand possedeVoisinBleu == 0
+      */
+      pileSommetsDepile(pile);
+    }
+  }
+
+  pileSommetsLibere(pile);
 
 }
 
