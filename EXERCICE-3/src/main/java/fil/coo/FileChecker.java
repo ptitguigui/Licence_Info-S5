46402e8afc8b9a426661afd08b2ed28f5537aea8
Lepretre Guillaume <lepretreg@a10p10.fil.univ-lille1.fr>
2017-11-21 12:23:01 +0100

    add class Filechecker

--- EXERCICE-3/src/main/java/fil/coo/FileChecker.java
+++ EXERCICE-3/src/main/java/fil/coo/FileChecker.java
@@ -10,28 +10,16 @@ import java.util.List;
 
 public class FileChecker {
 
-
+    private static final String FOLDER_PATH = "/plugins";
     private Timer timer;
     private FilenameFilter filenameFilter;
     private List<FileListener> listeners;
     private List<String> memory;
 
-    public FileChecker() {
+    public FileChecker(FilenameFilter filenameFilter) {
         initList();
         initTimer();
-        initFilenameFilter();
-    }
-
-    private void initFilenameFilter() {
-        filenameFilter = new FilenameFilter() {
-            @Override
-            public boolean accept(File file, String s) {
-                if(s.endsWith(".class")){
-                    return true;
-                }
-                return false;
-            }
-        };
+        this.filenameFilter = filenameFilter;
     }
 
     private void initTimer() {
@@ -43,15 +31,57 @@ public class FileChecker {
         memory = new ArrayList<>();
     }
 
-    public void start(){
+    public void start() {
         timer.start();
     }
 
     private void checkFiles() {
 
+        List<String> filenames = getFilenames();
+
+        checkNewFiles(filenames);
+        checkRemovedFiles(filenames);
     }
 
-    private class FileActionLister implements ActionListener{
+
+    private List<String> getFilenames() {
+        List<String> filenames = new ArrayList<>();
+        File dir = new File(FOLDER_PATH);
+        File[] files = dir.listFiles(this.filenameFilter);
+
+        for (File f : files) {
+            filenames.add(f.getName());
+        }
+        return filenames;
+    }
+
+    private void checkNewFiles(List<String> filenames) {
+
+        for (String filename : filenames) {
+            if (!(memory.contains(filename))) {
+                fireAdded();
+            }
+        }
+    }
+
+    private void fireAdded() {
+        //TODO
+    }
+
+
+    private void checkRemovedFiles(List<String> filenames) {
+        for (String filename : memory) {
+            if (!(filenames.contains(filename))) {
+                fireRemoved();
+            }
+        }
+    }
+
+    private void fireRemoved() {
+        //TODO
+    }
+
+    private class FileActionLister implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
