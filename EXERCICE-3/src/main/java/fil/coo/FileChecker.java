a6ff213dc1076d0b45680d2aa40d3e74217da5b9
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-11-22 15:36:44 +0100

    clean FileChecker

--- EXERCICE-3/src/main/java/fil/coo/FileChecker.java
+++ EXERCICE-3/src/main/java/fil/coo/FileChecker.java
@@ -6,20 +6,23 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class FileChecker implements FileListener{
+public class FileChecker implements FileListener {
 
-    private static final String FOLDER_PATH = "plugins";
+    private static final String REPOSITORY_PATH = "plugins";
+    
     private Timer timer;
-    private FilenameFilter filenameFilter;
+    private final FilenameFilter filenameFilter;
     private List<FileListener> listeners;
     private List<String> memory;
 
     public FileChecker(FilenameFilter filenameFilter) {
+        this.filenameFilter = filenameFilter;
+
         initList();
         initTimer();
-        this.filenameFilter = filenameFilter;
     }
 
     private void initTimer() {
@@ -35,48 +38,57 @@ public class FileChecker implements FileListener{
         timer.start();
     }
 
-    private void checkFiles() {
+    /**
+     * Checks for changes in the repository folder
+     */
+    private void checkChangedFiles() {
 
-        List<String> filenames = getFilenames();
+        List<String> filenames = getCurrentContents();
 
         checkNewFiles(filenames);
         checkRemovedFiles(filenames);
     }
 
 
-    private List<String> getFilenames() {
-        List<String> filenames = new ArrayList<>();
-        File dir = new File(FOLDER_PATH);
-        File[] files = dir.listFiles(this.filenameFilter);
-
-        for (File f : files) {
-            filenames.add(f.getName());
-        }
-        return filenames;
+    /**
+     * @return the list of current files in {@link #REPOSITORY_PATH}
+     */
+    private List<String> getCurrentContents() {
+        File dir = new File(REPOSITORY_PATH);
+        return Arrays.asList(dir.list(this.filenameFilter));
     }
 
-    private void checkNewFiles(List<String> filenames) {
-
-        for (String filename : filenames) {
+    /**
+     * Iterates over currentFiles and calls {@link #fileAdded(FileEvent)} if the file did NOT exist
+     * previously
+     *
+     * @param currentFiles the list of files currently in {@link #REPOSITORY_PATH}
+     */
+    private void checkNewFiles(List<String> currentFiles) {
+        for (String filename : currentFiles) {
             if (!(memory.contains(filename))) {
                 fireAdded(filename);
             }
         }
     }
 
-    private void fireAdded(String filename) {
-        FileEvent event = new FileEvent(filename);
-
-    }
-
-
-
-    private void checkRemovedFiles(List<String> filenames) {
-        for (String filename : memory) {
-            if (!(filenames.contains(filename))) {
+    /**
+     * Iterates over {@link #memory} and calls {@link #fileRemoved(FileEvent)}} if the file did exist
+     * previously
+     *
+     * @param currentFiles the list of files currently in {@link #REPOSITORY_PATH}
+     */
+    private void checkRemovedFiles(List<String> currentFiles) {
+        for (String preExistingFile : memory) {
+            if (!(currentFiles.contains(preExistingFile))) {
                 fireRemoved();
             }
         }
+    }
+
+    private void fireAdded(String filename) {
+        FileEvent event = new FileEvent(filename);
+        // TODO notify
     }
 
     private void fireRemoved() {
@@ -97,7 +109,7 @@ public class FileChecker implements FileListener{
 
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
-            checkFiles();
+            checkChangedFiles();
         }
     }
 }
