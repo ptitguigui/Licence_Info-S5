a2f76d7db273b259c5967b4ee4ac7c92579f78c5
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-11-27 17:18:55 +0100

    refactored PluginFilter

--- EXERCICE-4/src/main/java/fil/coo/PluginFilter.java
+++ EXERCICE-4/src/main/java/fil/coo/PluginFilter.java
@@ -1,7 +1,6 @@
 package fil.coo;
 
 import org.apache.log4j.Logger;
-import plugins.CesarCode;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -14,51 +13,80 @@ public class PluginFilter implements FilenameFilter {
 
     private Logger logger = Logger.getLogger(PluginFilter.class.getSimpleName());
 
+    private static final String PLUGIN_PACKAGE = "plugins.";
     private static final String EXTENSION_CLASS = ".class";
 
 
+    /**
+     * @param dir      the directory which contains filename
+     * @param filename the name of the file to be considered
+     * @return if the file points to a plugin
+     */
     @Override
-    public boolean accept(File file, String s) {
-        // TODO
-
-        Class<?> aClass = canBeInstantiate(s);
-        boolean classExists = aClass != null;
-
-
-        return endWithClass(s) && classExists && canGetConstructor(aClass);
-    }
-
-    private boolean canGetConstructor(Class<?> s) {
-
-        try {
-            Constructor<?> constructor = s.getConstructor(null);
-        } catch (NoSuchMethodException e) {
+    public boolean accept(File dir, String filename) {
+        if (!fileCanBeClass(filename)) {
             return false;
         }
-
-        return true;
+        return isClassInstanceAPlugin(filename);
     }
 
-    private Class<?> canBeInstantiate(String s) {
-        Class<?> c;
+    /**
+     * @param filename the name of the file to be treated
+     * @return if filename ends with {@link #EXTENSION_CLASS}, is long enough for an actual class name and starts
+     * with an uppercase
+     */
+    private boolean fileCanBeClass(String filename) {
+        return filename.endsWith(EXTENSION_CLASS) && filename.length() > EXTENSION_CLASS.length() && Character
+                .isUpperCase(filename.charAt(0));
+    }
+
+    /**
+     * @param filename the name of the file to be treated
+     * @return if a {@link Class} can be instantiated from the {@link #PLUGIN_PACKAGE} and has an empty constructor
+     */
+    private boolean isClassInstanceAPlugin(String filename) {
+        Class<?> classFromFile = getClassFromFile(filename);
+        return classFromFile != null && canGetConstructor(classFromFile);
+    }
+
+    /**
+     * @param filename the name of the file to be treated
+     * @return the {@link Class} associated to the filename in the {@link #PLUGIN_PACKAGE}
+     */
+    private Class<?> getClassFromFile(String filename) {
+
+        Class<?> instantiatedClass;
         try {
-            c = Class.forName("plugins." + getClassName(s));
+            instantiatedClass = Class.forName(PLUGIN_PACKAGE + getClassNameFromFile(filename));
         } catch (ClassNotFoundException | ClassFormatError e) {
             logger.debug(e);
             return null;
         }
-        return c;
+        return instantiatedClass;
     }
 
-    private String getClassName(String s) {
-        if(!(s.length() > EXTENSION_CLASS.length())){
+    /**
+     * @param classz the class to consider
+     * @return if classz has an empty constructor
+     */
+    private boolean canGetConstructor(Class<?> classz) {
+        try {
+            Constructor<?> constructor = classz.getConstructor(null);
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param filename the name of the file to be treated
+     * @return the java classname from filename
+     */
+    private String getClassNameFromFile(String filename) {
+        if (!fileCanBeClass(filename)) {
             return null;
         }
-        return s.substring(0, s.length() - EXTENSION_CLASS.length());
-    }
-
-    private boolean endWithClass(String s) {
-        return s.endsWith(EXTENSION_CLASS);
+        return filename.substring(0, filename.length() - EXTENSION_CLASS.length());
     }
 
 }
