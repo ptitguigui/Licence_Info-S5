d2f55ad003ccb8de2c139cfe3552a298e7cb0a17
Lepretre Guillaume <lepretreg@a10p12.fil.univ-lille1.fr>
2017-11-27 16:14:29 +0100

    implements class.Forname method

--- EXERCICE-4/src/main/java/fil/coo/PluginFilter.java
+++ EXERCICE-4/src/main/java/fil/coo/PluginFilter.java
@@ -8,11 +8,27 @@ import java.io.FilenameFilter;
  */
 public class PluginFilter implements FilenameFilter {
 
+    private static final String EXTENSION_CLASS = ".class";
+
     @Override
     public boolean accept(File file, String s) {
         // TODO
-        
-        return s.endsWith(".class");
+
+        return endWithClass(s) && canBeInstantiate(s);
+    }
+
+    private boolean canBeInstantiate(String s) {
+        String className = s.substring(0, s.length()-EXTENSION_CLASS.length());
+        try {
+            Class<?> c = Class.forName(s);
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean endWithClass(String s) {
+        return s.endsWith(EXTENSION_CLASS);
     }
 
 }
