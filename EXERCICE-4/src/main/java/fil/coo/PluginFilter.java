12fe4146047a13b33b9fe5e6e63f8471c7f7c6db
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-11-27 15:31:44 +0100

    added .class to test with

--- EXERCICE-4/src/main/java/fil/coo/PluginFilter.java
+++ EXERCICE-4/src/main/java/fil/coo/PluginFilter.java
@@ -3,6 +3,9 @@ package fil.coo;
 import java.io.File;
 import java.io.FilenameFilter;
 
+/**
+ * An implementation of {@link FilenameFilter} that only accepts compiled java .class files
+ */
 public class PluginFilter implements FilenameFilter {
 
     @Override
--- EXERCICE-4/src/test/java/fil/coo/PluginFilterTest.java
+++ EXERCICE-4/src/test/java/fil/coo/PluginFilterTest.java
@@ -1,7 +1,16 @@
 package fil.coo;
 
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class PluginFilterTest {
+
+    protected PluginFilter pluginFilter;
+
+    @Before
+    public void setupPluginFilter() {
+        this.pluginFilter = new PluginFilter();
+    }
 
 }
