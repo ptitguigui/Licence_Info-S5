b98f7b9d82075c2226b965fef3fd0edb31c560e4
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-11-27 15:21:03 +0100

    skeleton for PluginFilter

---
+++ EXERCICE-4/src/main/java/fil/coo/PluginFilter.java
@@ -0,0 +1,14 @@
+package fil.coo;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class PluginFilter implements FilenameFilter {
+
+    @Override
+    public boolean accept(File file, String s) {
+        // TODO
+        return false;
+    }
+
+}
