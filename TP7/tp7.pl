dad57b6233074e3dedeccd7a431d3b2bf853fdf6
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2017-11-27 14:05:26 +0100

    TP7 Q12

--- TP7/tp7.pl
+++ TP7/tp7.pl
@@ -73,5 +73,74 @@ schemaQ8([propertyid,countyname,lotno,area,price,taxrate]).
 fdsQ8([ [[propertyid],[countyname, lotno, area, price, taxrate]], [[countyname, lotno],[propertyid, area, price, taxrate]],[[countyname],[taxrate]] ]).
 decompQ8([[propertyis,area,lotno],[area,countyname],[area,price],[countyname,taxrate] ]).
 
-% tests lossless decomposition
+% tests if decompQ8 is a lossless decomposition of schemaQ8
 % schemaQ8(R), fdsQ8(F), decompQ8(D), ljd(R,F,D).
+
+
+% Q9
+schemaQ9([a,b,c,d,e,f,h,i]).
+fdsQ9([ [[a,b],[c]], [[a],[d, e]],[[b],[f]] ,[[f],[g,h]] ,[[d],[i,j]] ]).
+
+% prints all the candidate keys of R
+% schemaQ9(R), fdsQ9(F), candkey(R,F,K).
+
+% puts the the minimal cover of R in MinF
+% schemaQ9(R),fdsQ9(F),mincover(R,F,MinF).
+
+% returns a decomposition of R into 3NF in the R3NF variable
+% schemaQ9(R), fdsQ9(F), threenf(R,F,R3NF).
+
+% Q10
+schemaQ10([a,b,c,d,e,f,h,i]).
+fdsQ10([ [[a,b],[c]], [[b,d],[e, f]],[[b],[f]] ,[[f],[g,h]] ,[[d],[i,j]] ]).
+
+% prints all the candidate keys of R
+% schemaQ10(R), fdsQ10(F), candkey(R,F,K).
+
+% puts the the minimal cover of R in MinF
+% schemaQ10(R),fdsQ10(F),mincover(R,F,MinF).
+
+% returns a decomposition of R into 3NF in the R3NF variable
+% schemaQ10(R), fdsQ10(F), threenf(R,F,R3NF).
+
+% Q11
+schemaQ11([a,b,c,d,e,f,h,i]).
+fdsQ11([ [[a,b],[c]], [[a],[d, e]],[[b],[f]] ,[[f],[g,h]] ,[[d],[i,j]] ]).
+
+% returns in D a decomposition of R into BCNF.
+% schemaQ11(R), fdsQ11(F), bcnf(R,F,D).
+
+% with new functional dependancies:
+fdsQ11_2([ [[a,b],[c]], [[b,d],[e, f]],[[b],[f]] ,[[f],[g,h]] ,[[d],[i,j]] ]).
+% returns in D a decomposition of R into BCNF.
+% schemaQ11(R), fdsQ11_2(F), bcnf(R,F,D).
+
+% Q12
+schemaQ12([a,b,c,d,e]).
+fdsQ12([ [[a,b],[c]], [[c,d],[e]],[[d,e],[b]] ]).
+
+% prints all the candidate keys of R
+% schemaQ12(R), fdsQ12(F), candkey(R,F,K).
+
+% returns a decomposition of R into 3NF in the R3NF variable
+% schemaQ12(R), fdsQ12(F), threenf(R,F,R3NF).
+
+% returns in D a decomposition of R into BCNF.
+% schemaQ12(R), fdsQ12(F), bcnf(R,F,D).
+
+% Q12.2
+schemaQ12_2([courseno,secno,offeringdept,credithours,courselevel,
+instructorssn,semester,year, dayshours,roomno,
+noofstudents]).
+fdsQ12_2([ [[courseno,[offeringdept, credithours, courselevel]],
+ [[courseno, secno, semester, year],[dayshours, roomno, noofstudents, instructorssn]],
+ [[roomno, dayshours, semester, year],[instructorssn, courseno, secno]] ]).
+
+ % prints all the candidate keys of R
+ % schemaQ12_2(R), fdsQ12_2(F), candkey(R,F,K).
+
+ % returns a decomposition of R into 3NF in the R3NF variable
+ % schemaQ12_2(R), fdsQ12_2(F), threenf(R,F,R3NF).
+
+ % returns in D a decomposition of R into BCNF.
+ % schemaQ12_2(R), fdsQ12_2(F), bcnf(R,F,D).
