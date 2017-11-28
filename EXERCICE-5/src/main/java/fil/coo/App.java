5020177c810d0d39e46f8612a558d4c0a233e266
Lepretre Guillaume <lepretreg@a11p15.fil.univ-lille1.fr>
2017-11-28 16:27:37 +0100

    refactoring

--- EXERCICE-5/src/main/java/fil/coo/App.java
+++ EXERCICE-5/src/main/java/fil/coo/App.java
@@ -1,4 +1,11 @@
 package fil.coo;
 
+import fil.coo.view.impl.EditorFrame;
+
 public class App {
+
+    public static void main(String[] args) {
+        EditorFrame e = new EditorFrame();
+        e.setVisible(true);
+    }
 }
--- EXERCICE-5/src/main/java/fil/coo/controller/AbstractController.java
+++ EXERCICE-5/src/main/java/fil/coo/controller/AbstractController.java
@@ -2,7 +2,7 @@ package fil.coo.controller;
 
 import fil.coo.model.AbstractModel;
 import fil.coo.view.AbstractView;
-import fil.coo.view.CustomJMenuItem;
+import fil.coo.view.impl.CustomJMenuItem;
 
 public class AbstractController {
 
