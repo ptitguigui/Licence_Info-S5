3e34f8dc735d0969f80ef930f1164d29297f56be
Lepretre Guillaume <lepretreg@a14p15.fil.univ-lille1.fr>
2018-01-08 16:29:26 +0100

    add frontiere

--- scripts/imageSlider.js
+++ scripts/imageSlider.js
@@ -4,7 +4,6 @@
  */
 var ImageSlider = function(aCanvas){
   this.myCanvas = aCanvas;
-  this.position = aCanvas.width/2;
 
   this.context = aCanvas.getContext("2d");
 
@@ -15,6 +14,9 @@ ImageSlider.prototype.init = function(){
   this.myCanvas.height = this.img1.height;
 
   this.dessine();
+  this.context.fillStyle = "rgb(255,0,0)";
+  this.context.fillRect(this.myCanvas.width/2,0,5,this.myCanvas.height);
+  this.myCanvas.addEventListener("keydown",keyAction);
 }
 
 ImageSlider.prototype.dessine = function(){
@@ -28,7 +30,29 @@ ImageSlider.prototype.initImages = function(imgAvant, imgApres){
   this.img1.src = imgAvant;
 
   this.img2 = new Image();
-  this.img2.addEventListener("load", this.init.bind(this), true);
+  this.img2.addEventListener("load", this.init.bind(this));
 
   this.img2.src = imgApres;
+}
+
+// la fonction d'écoute, comme toute fonction d'écoute elle dispose d'un paramètre 'event'
+var keyAction = function(event) {
+   // on peut placer ici du code qui est exécuté dans tous les cas avant le traitement des touches
+   switch (event.key) {
+      case "ArrowLeft":
+      case "Left":
+         //  écrire ici le code exécuté si la touche flèche gauche est appuyée
+      break;
+      case "ArrowRight":
+      case "Right":
+         // écrire ici le code exécuté si la touche flèche droite est appuyée
+      break;
+      case "X":       
+      case "x":       
+         // écrire ici le code exécuté si la touche "x" est appuyée
+      break;
+      default: return;
+   }
+   // on peut aussi écrire ici du code exécuté dans tous les cas après le traitement des touches
+   event.preventDefault();   // pour désactiver l'action éventuelle par défaut liée à la touche pressée
 }
