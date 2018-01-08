8af732b432ba00cbfc499b0bc4ecc8e0cbe00567
Lepretre Guillaume <lepretreg@a14p15.fil.univ-lille1.fr>
2018-01-08 16:22:24 +0100

    q1 ex2

--- scripts/imageSlider.js
+++ scripts/imageSlider.js
@@ -4,10 +4,31 @@
  */
 var ImageSlider = function(aCanvas){
   this.myCanvas = aCanvas;
-  this.position = myCanvas.getContext("2d");
+  this.position = aCanvas.width/2;
 
-  this.initImages("img1", "img2"){
-    
-  }
+  this.context = aCanvas.getContext("2d");
 
+}
+
+ImageSlider.prototype.init = function(){
+  this.myCanvas.width = this.img1.width;
+  this.myCanvas.height = this.img1.height;
+
+  this.dessine();
+}
+
+ImageSlider.prototype.dessine = function(){
+    this.context.drawImage(this.img1,0,0);
+    this.context.drawImage(this.img2,0,0);
+}
+
+ImageSlider.prototype.initImages = function(imgAvant, imgApres){
+
+  this.img1 = new Image();
+  this.img1.src = imgAvant;
+
+  this.img2 = new Image();
+  this.img2.addEventListener("load", this.init.bind(this), true);
+
+  this.img2.src = imgApres;
 }
