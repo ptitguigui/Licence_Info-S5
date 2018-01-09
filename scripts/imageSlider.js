ea0f07bdff6800d273f5d2163229f3473cab0fe4
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2018-01-09 13:03:32 +0100

    added key actions

--- scripts/imageSlider.js
+++ scripts/imageSlider.js
@@ -10,24 +10,6 @@ var ImageSlider = function(aCanvas){
 
 }
 
-ImageSlider.prototype.init = function(){
-  this.myCanvas.width = this.img1.width;
-  this.myCanvas.height = this.img1.height;
-
-  this.position = this.myCanvas.width/2;
-  this.dessine();
-
-  this.context.fillStyle = "rgb(255,0,0)";
-  this.context.fillRect(this.position,0,5,this.myCanvas.height);
-
-  this.myCanvas.addEventListener("keydown",keyAction);
-}
-
-ImageSlider.prototype.dessine = function(){
-    this.context.drawImage(this.img1, 0, 0, this.position, this.img1.height, 0,0, this.img1.width-this.position, this.img1.height);
-    this.context.drawImage(this.img2, this.position, 0, this.img2.width, this.img2.height, this.position,0, this.img2.width, this.img2.height);
-}
-
 ImageSlider.prototype.initImages = function(imgAvant, imgApres){
 
   this.img1 = new Image();
@@ -39,24 +21,50 @@ ImageSlider.prototype.initImages = function(imgAvant, imgApres){
   this.img2.src = imgApres;
 }
 
+ImageSlider.prototype.init = function(){
+  this.myCanvas.width = this.img1.width;
+  this.myCanvas.height = this.img1.height;
+  this.position = this.myCanvas.width/2;
+
+  this.dessine();
+
+  this.myCanvas.addEventListener("keydown",keyAction.bind(this));
+}
+
+ImageSlider.prototype.dessine = function(){
+
+  // ctx.drawImage(image, sx, sy, sWidth, sHeight,
+  //dx, dy, dWidth, dHeight);
+    this.context.clearRect(0,0, this.myCanvas.width, this.myCanvas.height);
+
+    this.context.drawImage(this.img1, 0, 0, this.position, this.img1.height,
+      0,0, this.position, this.img1.height);
+
+    this.context.drawImage(this.img2, this.position, 0, this.img2.width, this.img2.height,
+      this.position,0, this.img2.width, this.img2.height);
+
+    this.context.fillStyle = "rgb(255,0,0)";
+    this.context.fillRect(this.position,0,5,this.myCanvas.height);
+}
+
 // la fonction d'écoute, comme toute fonction d'écoute elle dispose d'un paramètre 'event'
 var keyAction = function(event) {
    // on peut placer ici du code qui est exécuté dans tous les cas avant le traitement des touches
    switch (event.key) {
       case "ArrowLeft":
       case "Left":
-         //  écrire ici le code exécuté si la touche flèche gauche est appuyée
+        this.position -= 5;
       break;
       case "ArrowRight":
       case "Right":
-         // écrire ici le code exécuté si la touche flèche droite est appuyée
+        this.position += 5;
       break;
       case "X":       
       case "x":       
-         // écrire ici le code exécuté si la touche "x" est appuyée
+        this.position = this.myCanvas.width/2;
       break;
       default: return;
    }
-   // on peut aussi écrire ici du code exécuté dans tous les cas après le traitement des touches
+  this.dessine();
    event.preventDefault();   // pour désactiver l'action éventuelle par défaut liée à la touche pressée
 }
