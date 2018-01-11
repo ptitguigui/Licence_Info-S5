20d0336de0209c1743c27314304d71dca11e8425
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2018-01-11 14:01:47 +0100

    q5

--- tp1.hs
+++ tp1.hs
@@ -3,3 +3,15 @@
 
 sommeDeXaY :: (Enum a, Num a) => a -> a -> a
 sommeDeXaY x y = sum[x..y]
+
+somme :: [Int] -> Int
+somme [] = 0
+somme (n:ns) = n + somme ns
+
+mylast :: [a] -> a
+mylast [] = error "liste vide"
+mylast xs = xs !! ((length xs) - 1)
+
+myinit :: [a] -> a
+myinit [] = error "liste vide"
+myinit xs = head xs
