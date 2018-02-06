2806db3f4bcf67dd2a7386c0436cd9d99eafc32e
Lepretre Guillaume <lepretreg@a14p15.fil.univ-lille1.fr>
2018-02-06 15:31:11 +0100

    q1 2 3

--- arbres.hs
+++ arbres.hs
@@ -1,2 +1,33 @@
+data Arbre coul val =  Noeud {
+                              coul :: coul,
+                              val :: val,
+                              gauche :: Arbre coul val,
+                              droite :: Arbre coul val
+                              }
+                      | Feuille deriving Show
+
+
+
+mapArbre :: (a -> b) -> Arbre c a -> Arbre c b
+mapArbre _ Feuille         = Feuille
+mapArbre f (Noeud c v g d) = Noeud c (f v) (mapArbre f g) (mapArbre f d)
+
+
+hauteur :: Arbre c v -> Int
+hauteur Feuille         = 0
+hauteur (Noeud _ _ g d) = if lg > ld
+                         then lg
+                         else ld
+
+                         where lg = 1 + hauteur g
+                               ld = 1 + hauteur d
+
+taille :: Arbre c v -> Integer
+taille Feuille = 0
+taille (Noeud _ _ g d) = 1 + taille g + taille d
+
+
+
+
 main :: IO ()
 main = undefined
