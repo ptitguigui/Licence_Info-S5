a1ea1117a184634c422fec66a8e8b8ead26b5ae3
Lepretre Guillaume <lepretreg@a14p15.fil.univ-lille1.fr>
2018-02-06 16:09:15 +0100

    peigne gauche + quickcheck

--- arbres.hs
+++ arbres.hs
@@ -1,3 +1,7 @@
+import Test.QuickCheck
+
+
+
 data Arbre coul val =  Noeud {
                               coul :: coul,
                               val :: val,
@@ -13,7 +17,7 @@ mapArbre _ Feuille         = Feuille
 mapArbre f (Noeud c v g d) = Noeud c (f v) (mapArbre f g) (mapArbre f d)
 
 
-hauteur :: Arbre c v -> Integer
+hauteur :: Arbre c v -> Int
 hauteur Feuille         = 0
 hauteur (Noeud _ _ g d) = if lg > ld
                          then lg
@@ -22,20 +26,26 @@ hauteur (Noeud _ _ g d) = if lg > ld
                          where lg = 1 + hauteur g
                                ld = 1 + hauteur d
 
-taille :: Arbre c v -> Integer
+taille :: Arbre c v -> Int
 taille Feuille = 0
 taille (Noeud _ _ g d) = 1 + taille g + taille d
 
-foldArbre :: Integer b => (a -> b -> b -> b) -> b -> Arbre c v -> b
-foldArbre f n Feuille         = n
-foldArbre f n (Noeud _ v g d) = f v (foldArbre f n g) (foldArbre f n d)
+dimension :: (a -> b -> b -> b) -> b -> Arbre c a -> b
+dimension f n Feuille         = n
+dimension f n (Noeud _ v g d) = f v (dimension f n g) (dimension f n d)
 
-hauteur' :: Arbre c v -> Integer
-hauteur' = foldArbre (\_ g d -> 1 + max g d ) 0
+hauteur' :: Arbre c v -> Int
+hauteur' = dimension (\_ g d -> 1 + max g d ) 0
 
-taille' :: Arbre c v -> Integer
-taille' = foldArbre (\_ g d -> 1 + g + d ) 0
+taille' :: Arbre c v -> Int
+taille' = dimension (\_ g d -> 1 + g + d ) 0
 
+peigneGauche :: [(c,a)] -> Arbre c a
+peigneGauche []     = Feuille
+peigneGauche ((x1,x2):xs) = (Noeud x1 x2 (peigneGauche xs) Feuille)
+
+prop_hauteurPeigne :: [(c,a)] -> Bool
+prop_hauteurPeigne xs = length xs == hauteur (peigneGauche xs)
 
 
 
