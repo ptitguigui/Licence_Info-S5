59021360c0890156fd6fdd5bd6a0429c6c1550a8
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2018-03-06 15:09:34 +0100

    espaceP

--- Interprete.hs
+++ Interprete.hs
@@ -1,2 +1,26 @@
-main :: IO ()
-main = _
+import Parser
+
+type Nom = String
+
+data Expression = Lam Nom Expression
+                | App Expression Expression
+                | Var Nom
+                | Lit Litteral
+                deriving (Show,Eq)
+
+data Litteral = Entier Integer
+              | Bool   Bool
+              deriving (Show,Eq)
+
+
+espacesP :: Parser ()
+espacesP = do many (car ' ')
+              pure (())
+
+
+
+
+
+
+--main :: IO ()
+--main = _
