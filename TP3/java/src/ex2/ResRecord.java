9a3ca6cc9453009233b4d6190d87f183d0d73e57
Caroni Christopher <christopher.caroni@etudiant.univ-lille1.fr>
2018-03-15 10:00:48 +0100

    create ResRecord class

---
+++ TP3/java/src/ex2/ResRecord.java
@@ -0,0 +1,21 @@
+package ex2;
+
+public class ResRecord {
+	
+	private byte[] data;
+	
+	public ResRecord(byte[] data) {
+		this.data = data;
+		getLength();
+	}
+	
+	private void getLength() {
+		// TODO
+	}
+	
+	public byte[] getRData() {
+		return null;
+	}
+	
+
+}
